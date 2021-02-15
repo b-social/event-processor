aedcf261149cd65f9029d4e532e8bd3e6b3183de
mykola-rozhok <mykola.rozhok@b-social.com>
2021-02-15 17:30:18 +0000

    [ACC-533] made the lock acquiring code more secure + removed a test which is no longer relevant
    authors: @mykola-rozhok

--- src/event_processor/processor/locking/locks.clj
+++ src/event_processor/processor/locking/locks.clj
@@ -7,37 +7,52 @@
   [database lock-id & body]
   `(let [db# (:handle ~database)
          datasource# (:datasource db#)
-         connection# (.getConnection datasource#)]
-     (try
-       (log/log-debug
-         {:lock-id ~lock-id}
-         "Acquiring lock")
-       (let [get-lock-statement#
-             (.prepareStatement
-               connection#
-               "SELECT pg_try_advisory_lock(?)")
+         release-lock# (fn [connection#]
+                         (let [release-lock-statement#
+                               (.prepareStatement
+                                 connection#
+                                 "SELECT pg_advisory_unlock(?)")]
+                           (.setInt release-lock-statement# 1 ~lock-id)
+                           (.execute release-lock-statement#)
+                           nil))]
+     (when-let [connection# (try
+                              (.getConnection datasource#)
+                              (catch Exception e#
+                                (log/log-warn
+                                  {:lock-id   ~lock-id
+                                   :exception e#}
+                                  "Failed to secure the connection object")
+                                nil))]
+       (try
+         (log/log-debug
+           {:lock-id ~lock-id}
+           "Acquiring lock")
+         (let [get-lock-statement#
+               (.prepareStatement
+                 connection#
+                 "SELECT pg_try_advisory_lock(?)")
 
-             _# (.setInt get-lock-statement# 1 ~lock-id)
-             result-set# (.executeQuery get-lock-statement#)
+               _# (.setInt get-lock-statement# 1 ~lock-id)
+               result-set# (.executeQuery get-lock-statement#)
 
-             _# (.first result-set#)
-             acquired# (.getBoolean result-set# 1)
+               _# (.first result-set#)
+               acquired# (.getBoolean result-set# 1)
 
-             _# (.close result-set#)]
+               _# (.close result-set#)]
 
-         (when acquired#
-           ~@body))
+           (when acquired#
+             (log/log-debug
+               {:lock-id ~lock-id}
+               "Lock acquired")
+             ~@body
+             (log/log-debug
+               {:lock-id ~lock-id}
+               "Releasing lock")
+             (release-lock# connection#)))
 
-       (finally
-         (do
-           (log/log-debug
-             {:lock-id ~lock-id}
-             "Releasing lock")
-           (let [release-lock-statement#
-                 (.prepareStatement
-                   connection#
-                   "SELECT pg_advisory_unlock(?)")]
-             (doto
-               release-lock-statement#
-               (.setInt 1 ~lock-id)
-               .execute)))))))
+         (catch Exception e#
+           (log/log-warn
+             {:lock-id   ~lock-id
+              :exception e#}
+             "Failed run the acquire lock method")
+           (release-lock# connection#))))))
