636d0cf12a23c36240d6daa4bf877955e6ff25db
Alex Parlett <alexparlett@gmail.com>
2021-10-28 12:11:16 +0100

    [TECH] Don't close the connection this is a pooled conn
    Authors: @alexparlett

--- src/event_processor/processor/locking/locks.clj
+++ src/event_processor/processor/locking/locks.clj
@@ -4,43 +4,42 @@
 (defn acquire-lock
   [connection lock-id]
   (log/with-timings "Acquiring Lock" {}
-    (let [get-lock-statement#
-          (.prepareStatement
-            connection
-            "SELECT pg_try_advisory_lock(?)")
+                    (let [get-lock-statement#
+                          (.prepareStatement
+                            connection
+                            "SELECT pg_try_advisory_lock(?)")
 
-          _# (.setInt get-lock-statement# 1 lock-id)
-          result-set# (.executeQuery get-lock-statement#)
+                          _# (.setInt get-lock-statement# 1 lock-id)
+                          result-set# (.executeQuery get-lock-statement#)
 
-          _# (.first result-set#)
-          acquired (.getBoolean result-set# 1)
+                          _# (.first result-set#)
+                          acquired (.getBoolean result-set# 1)
 
-          _# (.close result-set#)]
-      acquired)))
+                          _# (.close result-set#)]
+                      acquired)))
 
 (defn release-lock
   [connection lock-id]
   (log/with-timings "Releasing Lock" {}
-    (let [release-lock-statement#
-          (.prepareStatement
-            connection
-            "SELECT pg_advisory_unlock(?)")]
-      (.setInt release-lock-statement# 1 lock-id)
-      (.execute release-lock-statement#)
-      (.close connection)
-      nil)))
+                    (let [release-lock-statement#
+                          (.prepareStatement
+                            connection
+                            "SELECT pg_advisory_unlock(?)")]
+                      (.setInt release-lock-statement# 1 lock-id)
+                      (.execute release-lock-statement#)
+                      nil)))
 
 (defn get-connection
   [datasource lock-id]
   (log/with-timings "Getting Connection" {}
-    (try
-      (.getConnection datasource)
-      (catch Exception e
-        (log/log-warn
-          {:lock-id   lock-id
-           :exception e}
-          "Failed to secure the connection object")
-        nil))))
+                    (try
+                      (.getConnection datasource)
+                      (catch Exception e
+                        (log/log-warn
+                          {:lock-id   lock-id
+                           :exception e}
+                          "Failed to secure the connection object")
+                        nil))))
 
 (defmacro with-lock
   "Try to acquire a Postgres advisory lock on the database.
@@ -53,19 +52,19 @@
          (log/log-debug
            {:lock-id ~lock-id}
            "Acquiring lock")
-         (if-let [acquired# (acquire-lock connection# ~lock-id)]
-           (do (log/log-debug
-                 {:lock-id ~lock-id}
-                 "Lock acquired")
-               ~@body
+         (when-let [acquired# (acquire-lock connection# ~lock-id)]
+           (log/log-debug
+             {:lock-id ~lock-id}
+             "Lock acquired")
+           (try
+             ~@body
+             (finally
                (log/log-debug
                  {:lock-id ~lock-id}
                  "Releasing lock")
-               (release-lock connection# ~lock-id))
-           (.close connection#))
+               (release-lock connection# ~lock-id))))
          (catch Exception e#
            (log/log-warn
              {:lock-id   ~lock-id
               :exception e#}
-             "Failed run the acquire lock method")
-           (.close connection#))))))
+             "Failed run the acquire lock method"))))))
